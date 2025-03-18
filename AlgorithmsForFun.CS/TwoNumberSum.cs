ef9668bd711af80f0889862d6deca04f74e29f46
Justin Horner <justinh.horner@gmail.com>
2025-03-18 02:27:52 -0400

    Initial commit

---
+++ AlgorithmsForFun.CS/TwoNumberSum.cs
@@ -0,0 +1,55 @@
+﻿namespace AlgorithmsForFun.CS;
+
+public partial class Algorithms
+{
+    /// <summary>
+    /// O(n) solution with O(n) space that maintains a dictionary of calculated diffs to find the sum.
+    /// </summary>
+    /// <param name="array">A non-empty array of distinct integers</param>
+    /// <param name="targetSum">The target sum to locate</param>
+    /// <returns>An array containing the two numbers that equal the target sum</returns>
+    public static int[] TwoNumberSum(int[] array, int targetSum)
+    {
+        var prevDiffs = new Dictionary<int, bool>();
+        for (var i = 0; i < array.Length; i++)
+        {
+            var diff = targetSum - array[i];
+            if (prevDiffs.ContainsKey(array[i]))
+            {
+                return [array[i], diff];
+            }
+
+            prevDiffs.Add(diff, true);
+        }
+
+        return [];
+    }
+
+    /// <summary>
+    /// O(nLog(n)) with O(1) space solution that sorts the input array before finding target sum elements
+    /// find the sum.
+    /// </summary>
+    /// <param name="array">A non-empty array of distinct integers</param>
+    /// <param name="targetSum">The target sum to locate</param>
+    /// <returns>An array containing the two numbers that equal the target sum</returns>
+    public static int[] TwoNumberSumSorted(int[] array, int targetSum)
+    {
+        Array.Sort(array);
+        var leftIndex = 0;
+        var rightIndex = array.Length - 1;
+
+        while (leftIndex < rightIndex)
+        {
+            var sum = array[leftIndex] + array[rightIndex];
+            if (sum == targetSum)
+                return [array[leftIndex], array[rightIndex]];
+            
+            if (sum < targetSum)
+                leftIndex++;
+            else
+                rightIndex--;
+        }
+
+        return [];
+    }
+}
---
+++ AlgorithmsForFun.Test/AlgorithmsTests.cs
@@ -0,0 +1,32 @@
+﻿using AlgorithmsForFun.CS;
+
+namespace AlgorithmsForFun.Test;
+
+public class AlgorithmsTests 
+{
+    [Theory]
+    [InlineData(new[] {3, 5, -4, 8, 11, 1, -1, 6}, 10, new[] { -1, 11 })]
+    [InlineData(new[] {4, 6}, 10, new[] { 4, 6 })]
+    [InlineData(new[] {4, 6, 1}, 5, new[] { 4, 1 })]
+    [InlineData(new[] {4, 6, 1, -3}, 3, new[] { 6, -3 })]
+    [InlineData(new[] {14}, 15, new int[] { })]
+    public void TwoNumberSum(int[] input, int targetSum, int[] expectedResult)
+    {
+        var result = Algorithms.TwoNumberSum(input, targetSum);
+
+        Assert.True(expectedResult.OrderBy(x => x).SequenceEqual(result.OrderBy(x => x)));
+    }
+    
+    [Theory]
+    [InlineData(new[] {3, 5, -4, 8, 11, 1, -1, 6}, 10, new[] { -1, 11 })]
+    [InlineData(new[] {4, 6}, 10, new[] { 4, 6 })]
+    [InlineData(new[] {4, 6, 1}, 5, new[] { 4, 1 })]
+    [InlineData(new[] {4, 6, 1, -3}, 3, new[] { 6, -3 })]
+    [InlineData(new[] {14}, 15, new int[] { })]
+    public void TwoNumberSum_Sorted(int[] input, int targetSum, int[] expectedResult)
+    {
+        var result = Algorithms.TwoNumberSumSorted(input, targetSum);
+
+        Assert.True(expectedResult.OrderBy(x => x).SequenceEqual(result.OrderBy(x => x)));
+    }
+}
