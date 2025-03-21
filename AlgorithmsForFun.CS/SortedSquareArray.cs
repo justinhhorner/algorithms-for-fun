a0f44bedfd8e92bcc1371ef9b8904008a5af8637
Justin Horner <justinh.horner@gmail.com>
2025-03-21 09:44:20 -0400

    Add sorted square array

---
+++ AlgorithmsForFun.CS/SortedSquareArray.cs
@@ -0,0 +1,39 @@
+﻿namespace AlgorithmsForFun.CS;
+
+public partial class Algorithms 
+{
+   /// <summary>
+   ///  O(n) solution using left/right pointers to calculate abs values and place squared values from right to left
+   /// </summary>
+   /// <param name="array">A non-empty array of integers sorted in ascending order</param>
+   /// <returns>Array of the same length as `array` with squares of original integers in ascending order</returns>
+   public static int[] SortedSquareArray(int[] array)
+   {
+      var squaredArray = new int[array.Length];
+      
+      var leftIndex = 0;
+      var rightIndex = array.Length - 1;
+      var insertIndex = rightIndex;
+      
+      while (rightIndex >= leftIndex)
+      {
+         var leftValue = Math.Abs(array[leftIndex]);
+         var rightValue = Math.Abs(array[rightIndex]);
+
+         if (rightValue > leftValue)
+         {
+            squaredArray[insertIndex] = rightValue * rightValue;
+            rightIndex--;
+         }
+         else
+         {
+            squaredArray[insertIndex] = leftValue * leftValue;
+            leftIndex++;
+         }
+         
+         insertIndex--;
+      }
+
+      return squaredArray;
+   }
+}
--- AlgorithmsForFun.Test/AlgorithmsTests.cs
+++ AlgorithmsForFun.Test/AlgorithmsTests.cs
@@ -10,7 +10,7 @@ public class AlgorithmsTests
     [InlineData(new[] { 4, 6, 1 }, 5, new[] { 4, 1 })]
     [InlineData(new[] { 4, 6, 1, -3 }, 3, new[] { 6, -3 })]
     [InlineData(new[] { 14 }, 15, new int[] { })]
-    public void TwoNumberSum(int[] input, int targetSum, int[] expectedResult)
+    public void TwoNumberSumTest(int[] input, int targetSum, int[] expectedResult)
     {
         var result = Algorithms.TwoNumberSum(input, targetSum);
 
@@ -23,7 +23,7 @@ public class AlgorithmsTests
     [InlineData(new[] { 4, 6, 1 }, 5, new[] { 4, 1 })]
     [InlineData(new[] { 4, 6, 1, -3 }, 3, new[] { 6, -3 })]
     [InlineData(new[] { 14 }, 15, new int[] { })]
-    public void TwoNumberSum_Sorted(int[] input, int targetSum, int[] expectedResult)
+    public void TwoNumberSum_SortedTest(int[] input, int targetSum, int[] expectedResult)
     {
         var result = Algorithms.TwoNumberSumSorted(input, targetSum);
 
@@ -38,10 +38,24 @@ public class AlgorithmsTests
     [InlineData(new[] {1, 1, 1, 1, 1 }, new[] { 1, 1, 1})]
     [InlineData(new[] {1, 1, 6, 1 }, new[] { 1, 1, 1, 6 }, false)]
     [InlineData(new[] {5, 1, 22, 25, 6, -1, 8, 10}, new[] {5, 1, 22, 23, 6, -1, 8, 10}, false)]
-    public void ValidateSubsequence(int[] input, int[] sequence, bool expectedResult = true)
+    public void ValidateSubsequenceTest(int[] input, int[] sequence, bool expectedResult = true)
     {
         var result = Algorithms.IsValidSubsequence(input, sequence);
 
         Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData(new [] { -2, -1 }, new [] { 1, 4 })]
+    [InlineData(new [] { 1 }, new [] { 1 })]
+    [InlineData(new [] { 0 }, new [] { 0 })]
+    [InlineData(new [] { 1, 2 }, new [] { 1, 4 })]
+    [InlineData(new [] { -5, -4, -3, -2, -1 }, new [] { 1, 4, 9, 16, 25 })]
+    [InlineData(new [] { 1, 2, 3, 5, 6, 8, 9}, new [] { 1, 4, 9, 25, 36, 64, 81})]
+    public void SortedSquaredArrayTest(int[] input, int[] expectedResult)
+    {
+        var result = Algorithms.SortedSquareArray(input);
+        
+        Assert.True(expectedResult.SequenceEqual(result.OrderBy(x => x)));
     }
 }
